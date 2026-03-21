a29362d166fd774e3600757242b1a9eb86e1f390
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2026-03-21 14:10:56 +0000

    init: kickstart base with LSP, Neo-tree, catppuccin, trouble, conform

---
+++ lua/kickstart/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/conform.nvim',
+  event = 'BufWritePre',
+  opts = {
+    formatters_by_ft = {
+      c = { 'clang_format' },
+      python = { 'black' },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '<leader>e', ':Neotree toggle position=right<CR>', desc = 'NeoTree toggle', silent = true },
+  },
+  opts = {
+    window = {
+      position = 'right',
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
