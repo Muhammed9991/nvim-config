6e7acfcee0deffcaa7328dbdcb5b69dc5010834c
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2026-04-23 11:17:20 +0100

    add: set spelling

--- init.lua
+++ init.lua
@@ -90,6 +90,14 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'tex', 'latex', 'markdown' },
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_gb'
+  end,
+})
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
